374971963d330899c7c4bb0bd87e92d63d6571cf
Emre Yılmaz <emreyilmaz@Emres-MacBook-Pro.local>
2020-07-10 11:37:10 +0300

    Updates

---
+++ precache-manifest.18ee604c534c220273122cb82db8b2ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19777a19f2ce087d420cdedc396175fd",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ad603126e7326d519766",
+    "url": "/weather/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "86add8bef234fe75bd3c",
+    "url": "/weather/static/css/main.690b2670.chunk.css"
+  },
+  {
+    "revision": "ad603126e7326d519766",
+    "url": "/weather/static/js/2.6cb4993f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather/static/js/2.6cb4993f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86add8bef234fe75bd3c",
+    "url": "/weather/static/js/main.a452c7b2.chunk.js"
+  },
+  {
+    "revision": "2be3ac631eccf1677078",
+    "url": "/weather/static/js/runtime-main.691605ec.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather/static/media/bg.b6796c73.jpg"
+  }
+]);
